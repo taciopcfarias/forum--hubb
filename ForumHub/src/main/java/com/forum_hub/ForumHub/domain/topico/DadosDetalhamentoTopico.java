0c75295d6c767ed47d4c5a6175ce3f8439680d16
PEDRO LINS <pedro.hcl29@gmail.com>
2024-07-15 17:15:27 -0300

    commit with classes coments

--- ForumHub/src/main/java/com/forum_hub/ForumHub/domain/topico/DadosDetalhamentoTopico.java
+++ ForumHub/src/main/java/com/forum_hub/ForumHub/domain/topico/DadosDetalhamentoTopico.java
@@ -1,7 +1,7 @@
-package com.forum_hub.ForumHub.domain.topico;
-
-public record DadosDetalhamentoTopico(Long id,String titulo,String mensagem,String autor,String curso) {
-    public DadosDetalhamentoTopico(Topico topico) {
-        this(topico.getId(), topico.getTitulo(), topico.getMensagem(), topico.getAutor(), topico.getCurso());
-    }
-}
+//package com.forum_hub.ForumHub.domain.topico;
+//
+//public record DadosDetalhamentoTopico(Long id,String titulo,String mensagem,String autor,String curso) {
+//    public DadosDetalhamentoTopico(Topico topico) {
+//        this(topico.getId(), topico.getTitulo(), topico.getMensagem(), topico.getAutor(), topico.getCurso());
+//    }
+//}
